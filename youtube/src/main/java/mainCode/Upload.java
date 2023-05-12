51f7657935a51e39e15fc67d03b61067200a535b
Amar <Amar@DreamSkater.ht.home>
2023-05-12 10:05:55 -0400

    dsf

--- youtube/src/main/java/mainCode/Upload.java
+++ youtube/src/main/java/mainCode/Upload.java
@@ -4,12 +4,7 @@ public class Upload {
 	
 	public static void main(String[] args) {
 		
-		<class name="orangeHRM.ExcelDataProviderFacebook" />
-			<class name="orangeHRM.ExcelDataSupplier" />
-			<class name="orangeHRM.DataProviderFacebook" />
-			<class name="orangeHRM.DependenciesTestNG" />
-			<class name="orangeHRM.OHRM" />
-			<class name="orangeHRM.DataProviderTestNG" />
+	
 		
 	}
 
